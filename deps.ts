e0868ceba6fef952ace11e367cffef4e03f9946d
DjDeveloperr <djsandhu2005@gmail.com>
2021-11-28 19:06:06 +0530

    .env support

--- deps.ts
+++ deps.ts
@@ -5,3 +5,4 @@ export {
 } from "https://deno.land/x/harmony@v2.3.0/mod.ts";
 export type { UserPayload } from "https://deno.land/x/harmony@v2.3.0/mod.ts";
 export { MongoClient } from "https://deno.land/x/mongo@v0.28.0/mod.ts";
+import "https://deno.land/x/dotenv@v3.1.0/load.ts";
